3622704cf273630051a0a94cf4e03c608126edcb
robbmoss <106989139+robbmoss@users.noreply.github.com>
2024-09-24 15:48:12 -0700

    Add files via upload

---
+++ PFHDA_code_AD_2.2/a_gam.m
@@ -0,0 +1,7 @@
+function a_gamout = a_gam(xL)
+
+a_gamout = 4.2797*xL + 1.6216;
+
+end
+
+%this is the a term in the gamma distribution fit to D/AD data
---
+++ PFHDA_code_AD_2.2/trlnrnd.m
@@ -0,0 +1,22 @@
+function truncout = trlnrnd(mu, sigma, n)
+
+%monte carlo sampling for a truncated lognormal distribution.  zmax is the
+%maximum value where truncation occurs, here it is specified as AD = 15m.
+%n random numbers between 0 and zmax are generated and thrown into the
+%Inverted CDF for a truncated lognormal distribution, then pass them back.
+
+z = unifrnd(0, 1, 1, n);
+
+%truncout = exp(sqrt(2*sigma*sigma)*erfinv(2*z*(logncdf(log(15), mu, sigma)) - 1) + mu);
+
+epsilon_max=5;  %based on DBA's corrections 9/14/23
+
+cdf_min=logncdf(exp(mu-epsilon_max*sigma),mu,sigma);
+
+cdf_max=logncdf(exp(mu+epsilon_max*sigma),mu,sigma);
+
+truncout=exp(sigma*sqrt(2)*erfinv(2*(z-0.5)*(cdf_max-cdf_min))+mu);
+
+
+end
+
